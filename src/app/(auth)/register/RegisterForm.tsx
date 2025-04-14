641e31ed818a3b016f4306508142c5136fd5f23e
keezeem <azeemolajide7@gmail.com>
2025-04-14 10:55:52 +0100

    fixed bug in RegisterForm

--- src/app/(auth)/register/RegisterForm.tsx
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -4,19 +4,31 @@ import { registerUser } from '@/app/actions/authActions';
 import { registerSchema, RegisterSchema } from '@/lib/schemas/registerSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Card, CardHeader, CardBody, Button, Input } from '@nextui-org/react';
-import React from 'react'
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { GiPadlock } from 'react-icons/gi';
 
 export default function RegisterForm() {
     const { register, handleSubmit, setError, formState: { errors, isValid, isSubmitting } } = useForm<RegisterSchema>({
-        // resolver: zodResolver(registerSchema),
+        resolver: zodResolver(registerSchema),
         mode: 'onTouched'
     });
 
     const onSubmit = async (data: RegisterSchema) => {
         const result = await registerUser(data);
-        console.log(result);
+        if (result.status === 'success') {
+            console.log('User registered successfully');
+        } else {
+            if (Array.isArray(result.error)) {
+                result.error.forEach((e) => {
+                    const fieldName = e.path.join('.') as 'email' | 'name' | 'password';
+                    setError(fieldName, { message: e.message });
+                });
+            } else {
+                setError('root.serverError', { message: result.error });
+            }
+        }
+    };
 
     return (
         <Card className='w-2/5 mx-auto'>
@@ -71,6 +83,5 @@ export default function RegisterForm() {
                 </form>
             </CardBody>
         </Card>
-    )
-}
+    );
 }
