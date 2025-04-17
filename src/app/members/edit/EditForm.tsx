0da009ef3aed8600e3442e17303a7fde350f682d
keezeem <azeemolajide7@gmail.com>
2025-04-17 09:36:24 +0100

    Adding the edit member form Part 2

--- src/app/members/edit/EditForm.tsx
+++ src/app/members/edit/EditForm.tsx
@@ -2,8 +2,9 @@
 
 import { memberEditSchema, MemberEditSchema } from '@/lib/schemas/memberEditSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Input, Textarea } from '@nextui-org/react';
 import { Member } from '@prisma/client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form';
 
 type Props = {
@@ -17,9 +18,140 @@ export default function EditForm({ member }: Props) {
             resolver: zodResolver(memberEditSchema), 
             mode: 'onTouched'
         });
+
+        useEffect(() => {
+            if (member) {
+                reset({
+                    name: member.name,
+                    description: member.description,
+                    city: member.city,
+                    country: member.country
+                })
+            }
+        }, [member, reset])
+
+    const onSubmit = async (data: MemberEditSchema) => {
+        console.log(data)
+    }
   return (
-    <div>
-      EditForm
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-4'>
+         <Input
+            label='Name'
+            variant='bordered'
+            {...register('name')}
+            defaultValue={member.name}
+            isInvalid={!!errors.name}
+            errorMessage={errors.name?.message}
+        />
+
+        <Textarea
+            label='Description'
+            variant='bordered'
+            {...register('description')}
+            defaultValue={member.description}
+            isInvalid={!!errors.description}
+            errorMessage={errors.description?.message}
+            minRows={6}
+        />
+        <div className='flex flex-row gap-3'>
+            <Input
+                label='City'
+                variant='bordered'
+                {...register('city')}
+                defaultValue={member.city}
+                isInvalid={!!errors.city}
+                errorMessage={errors.city?.message}
+            />
+            <Input
+                label='Country'
+                variant='bordered'
+                {...register('country')}
+                defaultValue={member.country}
+                isInvalid={!!errors.country}
+                errorMessage={errors.country?.message}
+            />
+        </div>
+        <Button
+            type='submit'
+            className='flex self-end'
+            variant='solid'
+            isDisabled={!isValid || !isDirty}
+            isLoading={isSubmitting}
+            color='secondary'
+        >
+            Update profile
+        </Button>
+    </form>
   )
 }
+
+
+
+
+
+//     const onSubmit = async (data: MemberEditSchema) => {
+//         const nameUpdated = data.name !== member.name;
+//         const result = await updateMemberProfile(data, nameUpdated);
+
+//         if (result.status === 'success') {
+//             toast.success('Profile updated');
+//             router.refresh();
+//             reset({ ...data })
+//         } else {
+//             handleFormServerErrors(result, setError)
+//         }
+//     }
+
+//     return (
+//         <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-4'>
+//             <Input
+//                 label='Name'
+//                 variant='bordered'
+//                 {...register('name')}
+//                 defaultValue={member.name}
+//                 isInvalid={!!errors.name}
+//                 errorMessage={errors.name?.message}
+//             />
+//             <Textarea
+//                 label='Description'
+//                 variant='bordered'
+//                 {...register('description')}
+//                 defaultValue={member.description}
+//                 isInvalid={!!errors.description}
+//                 errorMessage={errors.description?.message}
+//                 minRows={6}
+//             />
+//             <div className='flex flex-row gap-3'>
+//                 <Input
+//                     label='City'
+//                     variant='bordered'
+//                     {...register('city')}
+//                     defaultValue={member.city}
+//                     isInvalid={!!errors.city}
+//                     errorMessage={errors.city?.message}
+//                 />
+//                 <Input
+//                     label='Country'
+//                     variant='bordered'
+//                     {...register('country')}
+//                     defaultValue={member.country}
+//                     isInvalid={!!errors.country}
+//                     errorMessage={errors.country?.message}
+//                 />
+//             </div>
+//             {errors.root?.serverError && (
+//                 <p className='text-danger text-sm'>{errors.root.serverError.message}</p>
+//             )}
+//             <Button
+//                 type='submit'
+//                 className='flex self-end'
+//                 variant='solid'
+//                 isDisabled={!isValid || !isDirty}
+//                 isLoading={isSubmitting}
+//                 color='secondary'
+//             >
+//                 Update profile
+//             </Button>
+//         </form>
+//     )
+// }
