17db4906181bd714a22e9447ee5623027a4abb38
keezeem <azeemolajide7@gmail.com>
2025-04-29 19:00:12 +0100

    Reinitialize repo with existing files

---
+++ src/app/actions/authActions.ts
@@ -0,0 +1,116 @@
+'use server';
+
+import { auth, signIn, signOut } from '@/auth';
+import { prisma } from '@/lib/prisma';
+import { LoginSchema } from '@/lib/schemas/loginSchema';
+import { ProfileSchema, RegisterSchema, combinedRegisterSchema, registerSchema } from '@/lib/schemas/registerSchema';
+import { generateToken, getTokenByToken } from '@/lib/tokens';
+import { ActionResult } from '@/types';
+import { TokenType, User } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+import { AuthError } from 'next-auth';
+
+export async function signInUser(data: LoginSchema): Promise<ActionResult<string>> {
+    try {
+        const existingUser = await getUserByEmail(data.email);
+
+        if (!existingUser || !existingUser.email) return { status: 'error', error: 'Invalid credentials' }
+
+        if (!existingUser.emailVerified) {
+            const token = await generateToken(existingUser.email, TokenType.VERIFICATION);
+
+           
+
+            return { status: 'error', error: 'Please verify your email address before logging in' }
+        }
+
+        const result = await signIn('credentials', {
+            email: data.email,
+            password: data.password,
+            redirect: false
+        });
+        console.log(result);
+
+        return { status: 'success', data: 'Logged in' }
+    } catch (error) {
+        console.log(error);
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return { status: 'error', error: 'Invalid credentials' }
+                default:
+                    return { status: 'error', error: 'Something went wrong' }
+            }
+        } else {
+            return { status: 'error', error: 'Something else went wrong' }
+        }
+    }
+}
+
+export async function signOutUser() {
+    await signOut({ redirectTo: '/' });
+}
+
+export async function registerUser(data: RegisterSchema): Promise<ActionResult<User>> {
+    try {
+        const validated = combinedRegisterSchema.safeParse(data);
+
+        if (!validated.success) {
+            return { status: 'error', error: validated.error.errors }
+        }
+
+        const { name, email, password, gender, description, dateOfBirth, city, country } = validated.data;
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const existingUser = await prisma.user.findUnique({
+            where: { email }
+        });
+
+        if (existingUser) return { status: 'error', error: 'User already exists' };
+
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                passwordHash: hashedPassword,
+                profileComplete: true,
+                member: {
+                    create: {
+                        name,
+                        description,
+                        city,
+                        country,
+                        dateOfBirth: new Date(dateOfBirth),
+                        gender
+                    }
+                }
+            }
+        });
+
+        const verificationToken = await generateToken(email, TokenType.VERIFICATION);
+
+
+        return { status: 'success', data: user }
+    } catch (error) {
+        console.log(error);
+        return { status: 'error', error: 'Something went wrong' }
+    }
+
+}
+export async function getUserByEmail(email: string) {
+    return prisma.user.findUnique({where: {email}});
+}
+
+export async function getUserById(id: string) {
+    return prisma.user.findUnique({where: {id}});
+}
+
+export async function getAuthUserId() {
+    const session = await auth();
+    const userId = session?.user?.id;
+
+    if (!userId) throw new Error('Unauthorised');
+
+    return userId;
+}
---
+++ src/components/NewMessageToast.tsx
@@ -0,0 +1,28 @@
+import { MessageDto } from '@/types'
+import Link from 'next/link'
+import React from 'react'
+import {Image} from '@nextui-org/react';
+import { transformImageUrl } from '@/lib/utils';
+
+type Props = {
+    message: MessageDto
+}
+
+export default function NewMessageToast({message}: Props) {
+  return (
+    <Link href={`/members/${message.senderId}/chat`} className='flex items-center'>
+        <div className='mr-2'>
+            <Image 
+                src={transformImageUrl(message.senderImage) || '/images/user.png'}
+                height={50}
+                width={50}
+                alt='Sender image'
+            />
+        </div>
+        <div className='flex flex-grow flex-col justify-center'>
+            <div className='font-semibold'>{message.senderName} sent you a message</div>
+            <div className='text-sm'>Click to view</div>
+        </div>
+    </Link>
+  )
+}
